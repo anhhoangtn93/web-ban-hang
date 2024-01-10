47659a3528e0ee2dffa75ad7c56546a8b93829a6
anhhoangtn93 <131422999+anhhoangtn93@users.noreply.github.com>
2024-01-11 00:15:50 +0700

    global header, footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header>
+        {/* Your header content goes here */}
+        <h1>Header</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
