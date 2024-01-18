dec340e3adc29bb6f00cef0b54a86a6ae1ea8d84
anhhoangtn93 <anhhoangtn93@gmail.com>
2024-01-18 17:54:16 +0700

    top header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,35 @@
 const Header = () => {
-    return (
-      <header>
-        {/* Your header content goes here */}
-        <h1>Header</h1>
-      </header>
-    );
-  };
-  
-  export default Header;
+  return (
+    <header>
+      <div className="wrap-header flex w-full h-auto flex-col items-center py-5 bg-[#2579f2] text-white">
+        <div className="wrap-top-header flex h-auto flex-col items-center max-w-[1240px] w-full px-5">
+          <div className="top-header flex h-auto flex-row justify-between items-center w-full">
+            <a href="#" className="logo p-2">logo</a>
+            <div className="search-box">
+              <input type="text" />
+            </div>
+            <div className="user-login">login</div>
+            <div className="cart">cart</div>
+          </div>
+
+          <div className="main-menu flex h-auto flex-row justify-between items-center w-full mt-5">
+            <a className="block" href="#">
+              m1
+            </a>
+            <a className="block" href="#">
+              m2
+            </a>
+            <a className="block" href="#">
+              m3
+            </a>
+            <a className="block" href="#">
+              m4
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
